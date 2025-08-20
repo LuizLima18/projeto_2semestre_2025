f3c3bae7a8650ac7f859438b3e43d51cbc5aa68c
Fatec <luiz.a.p.lima18@gmail.com>
2025-08-20 19:41:23 -0300

    post

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -2,8 +2,12 @@ package com.programacao.web.fatec.api_fatec.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.programacao.web.fatec.api_fatec.entities.Cliente;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -17,5 +21,10 @@ public class ClienteController {
     @GetMapping("/teste2/{nome}")
     public String testeCliente2(@PathVariable String nome){
         return nome;
+    }
+
+    @PostMapping("")
+    public String createCliente(@RequestBody Cliente cliente){
+        return cliente.toString();
     }
 }
